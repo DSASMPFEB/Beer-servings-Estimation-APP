cffff6287f1a0d2f8a38be67691774723c00dc2e
DSASMPFEB <annamarialopus4@gmail.com>
2025-06-24 19:54:08 +0530

    Initial commit

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import pickle
 import streamlit as st
 import numpy as np
 from os import path
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("Beer Servings Estimation App")
 
+df = pd.read_csv("data/beer-servings.csv") 
+
+st.subheader("Alcohol Consumption Overview")
+
+avg_servings = df.groupby("continent")[
+    ["beer_servings", "wine_servings", "spirit_servings"]].mean()
+
+st.markdown("**Average Alcohol Servings by Continent**")
+st.bar_chart(avg_servings)
+
+
+fig, ax = plt.subplots()
+sns.scatterplot(
+    data=df, x="beer_servings", y="total_litres_of_pure_alcohol", hue="continent", ax=ax)
+st.markdown("**BEER SERVINGS vs TOTAL ALCOHOL CONSUMPTION**")
+st.pyplot(fig)
 
 file_name = "lr_reg.pkl"
 with open(path.join("model", file_name), "rb") as f:
